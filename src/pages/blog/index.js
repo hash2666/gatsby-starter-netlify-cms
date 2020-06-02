d48c1194f868e02b437b627b8342c8810677493b
hush2666 <61306414+hash2666@users.noreply.github.com>
2020-06-02 22:45:23 +0900

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -17,7 +17,7 @@ export default class BlogIndexPage extends React.Component {
             className="has-text-weight-bold is-size-1"
             style={{
               boxShadow: '0.5rem 0 0 #f40, -0.5rem 0 0 #f40',
-              backgroundColor: '#fff',
+              backgroundColor: '#121212',
               color: 'white',
               padding: '1rem',
             }}
